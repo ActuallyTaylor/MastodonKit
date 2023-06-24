b4488ff4890a8d6972ae4848d91c348b5d39308e
ActuallyTaylor <zachary.lineman@gmail.com>
2023-06-23 17:21:51 -0800

    Added history

--- Sources/MastodonKit/Models/History.swift
+++ Sources/MastodonKit/Models/History.swift
@@ -1,8 +1,15 @@
 //
-//  File.swift
-//  
+//  History.swift
+//  MastodonKit
 //
 //  Created by Taylor Lineman on 6/23/23.
 //
 
-import Foundation
+public struct History: Codable, Hashable {
+    /// UNIX timestamp on midnight of the given day.
+    public let day: String
+    /// The counted usage of the tag within that day.
+    public let uses: String
+    ///The total of accounts using the tag within that day.
+    public let accounts: String
+}
